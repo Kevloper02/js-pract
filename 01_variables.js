358b7764536bd1d8f73b724654794214c08c5bd7
Kevloper02 <162672638+Kevloper02@users.noreply.github.com>
2024-07-27 12:27:41 +0000

    Changes to be committed: 	new file:   01_variables.js

---
+++ 01_variables.js
@@ -0,0 +1,11 @@
+const accountId = "12345"
+let accountEmail = "krish1234@gmail.com"
+var accountPassword = "1721233" // not usable due to block issues, kinda optional
+accountCity = "gujrat"
+let accountState;
+
+console.log(accountId);
+
+accountEmail = "krish4321@gmail.com"
+
+console.table([accountEmail, accountId, accountCity, accountPassword, accountState])
