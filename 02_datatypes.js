b755201d32f1353bdd6232fd2ae032d5a7881ed6
Kevloper02 <162672638+Kevloper02@users.noreply.github.com>
2024-07-27 12:55:31 +0000

    Changes to be committed: 	new file:   02_datatypes.js

---
+++ 02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // it will treat your code like a new code or newer version of code
+
+// alert( 3 + 3) //it will work only in browser like this, not in nodejs that we are using
+
+// integer => 2 to power 53
+// bigint //for big numbers
+// string = ""
+// boolean = true/false
+// null => standalone value
+// undefined // used when value is not defined
+
+console.log(typeof "hitesh");
+console.log(typeof null); //object type
+console.log(typeof undefined); //undefined type
