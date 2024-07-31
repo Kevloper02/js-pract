0d9d6514eb360ea656923ce0ce1a81140ae4d67d
Kevloper02 <162672638+Kevloper02@users.noreply.github.com>
2024-07-31 06:52:23 +0000

    Changes to be committed: 	new file:   04_dateandtime.js

---
+++ 04_dateandtime.js
@@ -0,0 +1,13 @@
+let myDate = new Date()
+
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString()); //hard to read
+// console.log(typeof myDate);
+
+// let myNewDate = new Date("01-14-2023")
+// console.log(myNewDate.toLocaleString());
+
+let MyTimeStamp = Date.now()
+console.log(MyTimeStamp);
