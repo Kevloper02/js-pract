b005472a6455d487d26d49c0ffa63ce054e4c3cb
Kevloper02 <162672638+Kevloper02@users.noreply.github.com>
2024-07-31 07:50:57 +0000

    Changes to be committed: 	modified:   05_array.js

--- 05_array.js
+++ 05_array.js
@@ -1,4 +1,10 @@
 const myArr = [1, 2, 3, 4, 5, true, "Krish"]
-console.log(myArr);
-console.log(typeof myArr);
+// console.log(myArr);
+// console.log(typeof myArr);
 // slice does not manipulate original array but splice does
+
+const marvel_heroes = ['hulk', 'ironman', 'spiderman']
+const dc_heroes = ['batman', 'flash', 'superman']
+
+const all_new_heroes = [...marvel_heroes, ...dc_heroes]
+console.log(all_new_heroes); //if used concat then it will merge it but spread will spread the arrays
